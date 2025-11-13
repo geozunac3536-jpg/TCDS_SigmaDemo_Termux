5d487f4e5abbbb12f55efaa898862d2a49b62620
Genaro Carrasco Ozuna <geozunac3536@gmail.com>
2025-11-13 14:21:15 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='tcds_sigma_demo',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    author='Genaro Carrasco Ozuna',
+    author_email='genarocarrasco.ozuna@gmail.com',
+    description='TCDS Σ-metrics Demo en Termux',
+    license='MIT / CC-BY-4.0',
+    keywords='TCDS sigma-metrics E-Veto',
+    url='https://github.com/geozunac3536-jpg/TCDS_SigmaDemo_Termux',
+)
